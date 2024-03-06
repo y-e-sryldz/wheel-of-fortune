943e0d6b9b4980bf1334006eb279e13fa87bf564
sari <emre242469@gmail.com>
2024-03-06 10:25:27 +0300

    first commit

---
+++ android/app/src/main/kotlin/com/example/draw_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.draw_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/screens/login.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class login extends StatefulWidget {
+  const login({super.key});
+
+  @override
+  State<login> createState() => _loginState();
+}
+
+class _loginState extends State<login> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage("assets/arka_plan.jpg"),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: Row(
+          mainAxisAlignment:
+              MainAxisAlignment.center, // Öğeleri yatay olarak ortalamak için
+          children: <Widget>[
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Text(
+                  'Neyi Seçmeli ?',
+                  style: TextStyle(
+                    fontSize: 50,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                  ),
+                ),
+                SizedBox(height: 40),
+                Text(
+                  "Eğer bir seçim yapmak  zorundaysan ve\nbirden  çok seçenek varsa bunu şansa bırak!",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(color: Colors.white, fontSize: 15),
+                ),
+                SizedBox(
+                  height: 30,
+                ),
+                Text(
+                  "Hangi Restoran?, Hangi Numara?, Ne Yemeli...\nLimitleri sen koy!",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(color: Colors.white),
+                ),
+                SizedBox(height: 30,),
+                IconButton(
+                  onPressed: () {
+                    print("naber lan kaşmer");
+                  },
+                  icon: Icon(CupertinoIcons.play_arrow,size: 80,),
+                  
+                )
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
